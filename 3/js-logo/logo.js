9b1e9892e2b9f9b1a236cdacb1fb97eff06785a4
Arseny <124520932+programmer228337@users.noreply.github.com>
2023-02-09 17:42:21 +0500

    Add files via upload

---
+++ 3/js-logo/logo.js
@@ -0,0 +1,3 @@
+Document.getElementById("insta").onclick = function() {
+    window.location='https://instagram.com/m.ars_videomaker?igshid=YmMyMTA2M2Y=';
+}
---
+++ 3/js-practise/practise.js
@@ -0,0 +1,26 @@
+let arr = [1, 2, 3, 4, 5];
+//console.log(arr.length)
+for (let i = 0; i <= arr.length -1; i++) {
+	console.log(arr[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
